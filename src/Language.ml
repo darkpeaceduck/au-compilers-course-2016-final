36504ad2d976d5fc1ca0807375b3183dcb1240e0
StasBel <stasbelyaev96@gmail.com>
2016-10-17 04:35:32 +0300

    Fix merge errors

---
+++ src/Language.ml
@@ -0,0 +1,93 @@
+open Ostap
+open Matcher
+
+module Expr =
+  struct
+
+    type t =
+      | Const of int
+      | Var   of string
+      | Binop of string * t * t
+
+    let eval_binop o l r =
+      let bool_to_int = function true -> 1 | _ -> 0 in
+      match o with
+      | "+" -> l + r
+      | "-" -> l - r
+      | "*" -> l * r
+      | "/" -> l / r
+      | "%" -> l mod r
+      | _ ->
+         let e = match o with
+           | "<=" -> l <= r
+           | "<" -> l < r
+           | "==" -> l == r
+           | "!=" -> l != r
+           | ">=" -> l >= r
+           | ">" -> l > r
+           | "&&" -> (l != 0) && (r != 0)
+           | "!!" -> (l != 0) || (r != 0)
+         in bool_to_int e
+
+  ostap (
+    parse:
+      l:andi suf:("!!" andi)* {
+        List.fold_left (fun l (op, r) -> Binop (Token.repr op, l, r)) l suf
+      }
+  | andi;
+    
+    andi:
+      l:cmpi suf:("&&" cmpi)* {
+          List.fold_left (fun l (op, r) -> Binop (Token.repr op, l, r)) l suf
+        }
+  | cmpi;
+    
+    cmpi:
+      l:addi suf:(("<=" | "<" | "==" | "!=" | ">=" | ">") addi)* {
+          List.fold_left (fun l (op, r) -> Binop (Token.repr op, l, r)) l suf
+        }
+  | addi;
+    
+    addi:
+      l:mulli suf:(("+" | "-") mulli)* {
+          List.fold_left (fun l (op, r) -> Binop (Token.repr op, l, r)) l suf
+        }
+  | mulli;
+    
+    mulli:
+      l:primary suf:(("*" | "/" | "%") primary)* {
+          List.fold_left (fun l (op, r) -> Binop (Token.repr op, l, r)) l suf
+        }
+  | primary;
+    
+    primary:
+      n:DECIMAL {Const n}
+  | x:IDENT   {Var   x}
+  | -"(" parse -")"
+  )
+  
+  end
+
+module Stmt =
+  struct
+
+    type t =
+      | Skip
+      | Read   of string
+      | Write  of Expr.t
+      | Assign of string * Expr.t
+      | Seq    of t * t
+
+  ostap (
+    parse: s:simple d:(-";" parse)? {
+                                    match d with None -> s | Some d -> Seq (s, d)
+                                  };
+    simple:
+      x:IDENT ":=" e:!(Expr.parse)     {Assign (x, e)}
+      | %"read"  "(" x:IDENT ")"         {Read x}
+      | %"write" "(" e:!(Expr.parse) ")" {Write e}
+      | %"skip"                          {Skip}
+    (*| %"if" e:!(Expr.parse) *)
+  )
+  
+  end

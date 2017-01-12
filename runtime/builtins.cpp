9154af66d2670ef1576fa97634af3f80d9338641
sanya1 <okay123456@yandex.ru>
2017-01-12 05:11:33 +0300

    autoformat source

--- runtime/builtins.cpp
+++ runtime/builtins.cpp
@@ -19,20 +19,20 @@ static map<void*, int> mem_type;
 // IO
 
 extern "C" {
-  
-  extern int Lread() {
-    int d;
-    printf("> ");
-    scanf("%d", &d);
-    MARK_PRIMITIVE;
-    return d;
-  }
-  
-  extern int Lwrite(int x) {
-    MARK_PRIMITIVE;
-    return printf("%d\n", x);
-  }
-  
+
+extern int Lread() {
+	int d;
+	printf("> ");
+	scanf("%d", &d);
+	MARK_PRIMITIVE;
+	return d;
+}
+
+extern int Lwrite(int x) {
+	MARK_PRIMITIVE;
+	return printf("%d\n", x);
+}
+
 }
 
 // STRING
@@ -46,102 +46,102 @@ extern void Tgc_ref(void* a, int vt, void* v, int nt, void* n);
 extern void Tgc_collect();
 }
 
-
 static void builtin_mark(void *ptr) {
 	Tgc_inc_ref(1, ptr);
 	Tgc_dec_ref(1, ptr);
 }
-    
+
 typedef struct {
-  int len;
-  char buf[];
+	int len;
+	char buf[];
 } string_t;
 typedef string_t *mstring;
 
 static mstring stralloc(int n) {
-  void* ptr = gc_malloc(sizeof(int) + n * sizeof(char));
-  mem_type[ptr] = 1;
-  mstring s = (mstring) ptr;
-  s->len = n;
-  return s;
+	void* ptr = gc_malloc(sizeof(int) + n * sizeof(char));
+	mem_type[ptr] = 1;
+	mstring s = (mstring) ptr;
+	s->len = n;
+	return s;
 }
 
 extern "C" {
 
 extern mstring Lstrmake(int n, int c) {
-  mstring s = stralloc(n);
-  for (int i = 0; i < n; i++) {
-    s->buf[i] = c;
-  }
-  MARK_PTR;
-  return s;
+	mstring s = stralloc(n);
+	for (int i = 0; i < n; i++) {
+		s->buf[i] = c;
+	}
+	MARK_PTR;
+	return s;
 }
 
 extern mstring Lstrset(mstring s, int i, int c) {
-  s->buf[i] = c;
-  MARK_PTR;
-  return s;
+	s->buf[i] = c;
+	MARK_PTR;
+	return s;
 }
 
 extern int Lstrget(mstring s, int i) {
-  int ret = s->buf[i];
-  builtin_mark((void*)s);
-  Tgc_collect();
-  MARK_PRIMITIVE;
-  return ret;
+	int ret = s->buf[i];
+	builtin_mark((void*) s);
+	Tgc_collect();
+	MARK_PRIMITIVE;
+	return ret;
 }
 
 extern mstring Lstrdup(mstring s) {
-  mstring d = stralloc(s->len);
-  strncpy(d->buf, s->buf, s->len);
-  builtin_mark((void*)s);
-  Tgc_collect();
-  MARK_PTR;
-  return d;
+	mstring d = stralloc(s->len);
+	strncpy(d->buf, s->buf, s->len);
+	builtin_mark((void*) s);
+	Tgc_collect();
+	MARK_PTR;
+	return d;
 }
 
 extern mstring Lstrcat(mstring s1, mstring s2) {
-  mstring r = stralloc(s1->len + s2->len);
-  strncpy(r->buf, s1->buf, s1->len);
-  strncpy(r->buf + s1->len, s2->buf, s2->len);
-  builtin_mark((void*)s1);
-  builtin_mark((void*)s2);
-  Tgc_collect();
-  MARK_PTR;
-  return r;
+	mstring r = stralloc(s1->len + s2->len);
+	strncpy(r->buf, s1->buf, s1->len);
+	strncpy(r->buf + s1->len, s2->buf, s2->len);
+	builtin_mark((void*) s1);
+	builtin_mark((void*) s2);
+	Tgc_collect();
+	MARK_PTR;
+	return r;
 }
 
 extern int Lstrcmp(mstring s1, mstring s2) {
-  int res = strncmp(s1->buf, s2->buf, MIN(s1->len, s2->len));
-  if (res == 0) res = s1->len - s2->len;
-  builtin_mark((void*)s1);
-	builtin_mark((void*)s2);
+	int res = strncmp(s1->buf, s2->buf, MIN(s1->len, s2->len));
+	if (res == 0)
+		res = s1->len - s2->len;
+	builtin_mark((void*) s1);
+	builtin_mark((void*) s2);
 	Tgc_collect();
-  MARK_PRIMITIVE;
-  if (res < 0) {
-    return -1;
-  } else if (res == 0) {
-    return 0;
-  } else {
-    return 1;
-  }
+	MARK_PRIMITIVE;
+	if (res < 0) {
+		return -1;
+	} else if (res == 0) {
+		return 0;
+	} else {
+		return 1;
+	}
 }
 
 extern int Lstrlen(mstring s) {
-  int ret = s->len;
-  builtin_mark((void*)s);
-  Tgc_collect();
-  MARK_PRIMITIVE;
-  return ret;
+	int ret = s->len;
+	builtin_mark((void*) s);
+	Tgc_collect();
+	MARK_PRIMITIVE;
+	return ret;
 }
 
 extern mstring Lstrsub(mstring s, int i, int l) {
-  mstring r = stralloc(l);
-  strncpy(r->buf, s->buf + i, l);
-  builtin_mark((void*)s);
-  	Tgc_collect();
-  MARK_PTR;
-  return r;
+	mstring r = stralloc(l);
+	strncpy(r->buf, s->buf + i, l);
+	builtin_mark((void*) s);
+	Tgc_collect();
+	MARK_PTR;
+	return r;
 }
 
 }
@@ -149,68 +149,67 @@ extern mstring Lstrsub(mstring s, int i, int l) {
 // ARRAY
 
 typedef struct {
-  int len;
-  void* buf[];
+	int len;
+	void* buf[];
 } array_t;
 typedef array_t *marray;
 
 static marray arralloc(int n) {
-  marray a = (marray) gc_malloc(sizeof(int) + n * sizeof(void*));
-  a->len = n;
-  return a;
+	marray a = (marray) gc_malloc(sizeof(int) + n * sizeof(void*));
+	a->len = n;
+	return a;
 }
-
 
 extern "C" {
 
 extern int Larrlen(marray a) {
-  int ret = a->len;
-  builtin_mark((void*)a);
-  Tgc_collect();
-  MARK_PRIMITIVE;
-  return ret;
+	int ret = a->len;
+	builtin_mark((void*) a);
+	Tgc_collect();
+	MARK_PRIMITIVE;
+	return ret;
 }
 
 extern marray Larrmake(int n, int v) {
-  marray a = arralloc(n);
-  mem_type[(void*)a] = 2;
-  for (int i = 0; i < n; i++) {
-    a->buf[i] = (void*)v;
-  }
-  MARK_PTR;
-  return a;
+	marray a = arralloc(n);
+	mem_type[(void*) a] = 2;
+	for (int i = 0; i < n; i++) {
+		a->buf[i] = (void*) v;
+	}
+	MARK_PTR;
+	return a;
 }
 
 extern marray LArrmake(int n, void* ptr) {
-  marray a = arralloc(n);
-  Tgc_inc_ref(1, (void*)a);
-  mem_type[(void*)a] = 3;
-  for (int i = 0; i < n; i++) {
-    a->buf[i] = ptr;
-    Tgc_ref((void*)a, 0, NULL, 1, ptr);
-  }
+	marray a = arralloc(n);
+	Tgc_inc_ref(1, (void*) a);
+	mem_type[(void*) a] = 3;
+	for (int i = 0; i < n; i++) {
+		a->buf[i] = ptr;
+		Tgc_ref((void*) a, 0, NULL, 1, ptr);
+	}
 
-  builtin_mark(ptr);
-  Tgc_collect();
-  Tgc_dec_ref(1, (void*)a);
+	builtin_mark(ptr);
+	Tgc_collect();
+	Tgc_dec_ref(1, (void*) a);
 
-  MARK_PTR;
-  return a;
+	MARK_PTR;
+	return a;
 }
 
 extern void* Larrget(marray a, int n) {
-  void * ret = a->buf[n];
-  int memt = mem_type[a];
-  Tgc_inc_ref(memt, (void*)ret);
-  builtin_mark((void*)a);
-  Tgc_collect();
-  Tgc_dec_ref(memt, (void*)ret);
-  if (memt == 2) {
-    MARK_PRIMITIVE;
-  } else {
-    MARK_PTR;
-  }
-  return ret;
+	void * ret = a->buf[n];
+	int memt = mem_type[a];
+	Tgc_inc_ref(memt, (void*) ret);
+	builtin_mark((void*) a);
+	Tgc_collect();
+	Tgc_dec_ref(memt, (void*) ret);
+	if (memt == 2) {
+		MARK_PRIMITIVE;
+	} else {
+		MARK_PTR;
+	}
+	return ret;
 }
 
 }
--- runtime/collector.cpp
+++ runtime/collector.cpp
@@ -11,124 +11,127 @@ using namespace std;
 
 static set<void*> free_q;
 
-class RegisterItem{
-  int refs;
-  void* protect;
-  multiset<RegisterItem*> sub_objects;
+class RegisterItem {
+	int refs;
+	void* protect;
+	multiset<RegisterItem*> sub_objects;
 public:
-  RegisterItem() {}
-  RegisterItem(void * ptr) : protect(ptr), refs(0), sub_objects() {}
-  void inc_ref() {
-	/* anonymous fun res case */
-	if (this->refs == 0) {
-		if (free_q.count(this->protect))
-			free_q.erase(this->protect);
-		for (auto item : this->sub_objects)
-			item->inc_ref();
+	RegisterItem() {
 	}
-    this->refs++;
-  }
-  void dec_ref() {
-    this->refs--;
-    if (this->refs == 0) {
-      for(auto item : this->sub_objects) {
-    	  item->dec_ref();
-      }
-      free_q.insert(this->protect);
-    }
-  }
-  void depency(RegisterItem *obj) {
-    sub_objects.insert(obj);
-  }
-  void remove_depency(RegisterItem * ptr) {
-	  auto itr = sub_objects.find(ptr);
-	  if(itr != sub_objects.end()){
-		  sub_objects.erase(itr);
-	  }
-  }
-  int refs_cnt() {
-    return this->refs;
-  }
+	RegisterItem(void * ptr) :
+			protect(ptr), refs(0), sub_objects() {
+	}
+	void inc_ref() {
+		/* anonymous fun res case */
+		if (this->refs == 0) {
+			if (free_q.count(this->protect))
+				free_q.erase(this->protect);
+			for (auto item : this->sub_objects)
+				item->inc_ref();
+		}
+		this->refs++;
+	}
+	void dec_ref() {
+		this->refs--;
+		if (this->refs == 0) {
+			for (auto item : this->sub_objects) {
+				item->dec_ref();
+			}
+			free_q.insert(this->protect);
+		}
+	}
+	void depency(RegisterItem *obj) {
+		sub_objects.insert(obj);
+	}
+	void remove_depency(RegisterItem * ptr) {
+		auto itr = sub_objects.find(ptr);
+		if (itr != sub_objects.end()) {
+			sub_objects.erase(itr);
+		}
+	}
+	int refs_cnt() {
+		return this->refs;
+	}
 };
 
 static map<void*, RegisterItem *> registry;
 
 extern void* gc_malloc(size_t size) {
-  void* ptr = malloc(size);
-  registry[ptr] = new RegisterItem(ptr);
-  return ptr;
+	void* ptr = malloc(size);
+	registry[ptr] = new RegisterItem(ptr);
+	return ptr;
 }
 
 static int is_valid(int t, void* p) {
-  if (t != 0 && registry.find(p) != registry.end()) {
-    return 1;
-  } else {
-    return 0;
-  }
+	if (t != 0 && registry.find(p) != registry.end()) {
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
 extern "C" {
 
-  /**
-   *
-   */
-  /**
-   * assign
-   * t = 0 for primitive, 1 otherwise (means array or string)
-   */
-  extern void Tgc_inc_ref(int t, void* p) {
-    if (is_valid(t, p)) {
-      registry[p]->inc_ref();
-    }
-  }
+/**
+ *
+ */
+/**
+ * assign
+ * t = 0 for primitive, 1 otherwise (means array or string)
+ */
+extern void Tgc_inc_ref(int t, void* p) {
+	if (is_valid(t, p)) {
+		registry[p]->inc_ref();
+	}
+}
 
-  extern void Tgc_dec_ref(int t, void* p);
+extern void Tgc_dec_ref(int t, void* p);
 
-  /**
-   * arr assign
-   * a for array, v for prev value, n for new value
-   */
-  extern void Tgc_ref(void* a, int vt, void* v, int nt, void* n) {
+/**
+ * arr assign
+ * a for array, v for prev value, n for new value
+ */
+extern void Tgc_ref(void* a, int vt, void* v, int nt, void* n) {
 	if (v != n) {
 		Tgc_dec_ref(vt, v);
 		if (is_valid(vt, v))
-		if (registry.count(v))
-			registry[a]->remove_depency(registry[v]);
+			if (registry.count(v))
+				registry[a]->remove_depency(registry[v]);
 	}
-    Tgc_inc_ref(nt, n);
-    if (is_valid(nt, n)) {
-      registry[a]->depency(registry[n]);
-    }
-  }
+	Tgc_inc_ref(nt, n);
+	if (is_valid(nt, n)) {
+		registry[a]->depency(registry[n]);
+	}
+}
 
-  /**
-   * before Tgc_collect
-   */
-  extern void Tgc_dec_ref(int t, void* p) {
-    if (is_valid(t, p)) {
-      registry[p]->dec_ref();
-    }
-  }
+/**
+ * before Tgc_collect
+ */
+extern void Tgc_dec_ref(int t, void* p) {
+	if (is_valid(t, p)) {
+		registry[p]->dec_ref();
+	}
+}
 
-  /**
-   * ...
-   */
-  extern void Tgc_clear_q() {
-    free_q.clear();
-  }
+/**
+ * ...
+ */
+extern void Tgc_clear_q() {
+	free_q.clear();
+}
 
-  /**
-   * before ret
-   */
-  extern void Tgc_collect() {
-    for(auto iter : free_q) {
-      void * ptr = iter;
-      auto it = registry.find(ptr);
-      delete it->second;
-      registry.erase(it);
-      free(ptr);
-    }
-    Tgc_clear_q();
-  }
+/**
+ * before ret
+ */
+extern void Tgc_collect() {
+	for (auto iter : free_q) {
+		void * ptr = iter;
+		auto it = registry.find(ptr);
+		delete it->second;
+		registry.erase(it);
+		free(ptr);
+	}
+	Tgc_clear_q();
+}
 
 }
